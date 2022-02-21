f8a584cdc69eb9b170ef165d7e853e6b5598b3d0
mohamed <melsayed.se@gmail.com>
2022-02-21 23:21:39 +0200

    Refactored socket connection in ClientServerHandler. Added signUp functionality to ClientServerHandler

--- src/main/java/com/example/tictactoe/ClientServerHandler.java
+++ src/main/java/com/example/tictactoe/ClientServerHandler.java
@@ -2,6 +2,7 @@ package com.example.tictactoe;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import javafx.css.Match;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -10,6 +11,8 @@ import java.math.BigInteger;
 import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ClientServerHandler {
     private static final String SERVER_ADDRESS = "3.70.169.200";
@@ -18,8 +21,7 @@ public class ClientServerHandler {
     private static DataOutputStream dataOutputStream;
     private static Socket socket;
 
-    public static String signIn(String userName, String password){
-        String hashedPassword = hashPassword(password);
+    public static void connectSocket(){
         if(socket == null || socket.isClosed()){
             try {
                 socket = new Socket(SERVER_ADDRESS, Integer.parseInt(SERVER_PORT));
@@ -29,13 +31,46 @@ public class ClientServerHandler {
                 e.printStackTrace();
             }
         }
-        // Build Json payload
-        JsonObject jsonSignIn=new JsonObject();
-        jsonSignIn.addProperty("type","login");
-        jsonSignIn.addProperty("username",userName);
-        jsonSignIn.addProperty("password", hashedPassword);
+    }
+
+    public static boolean signUp(String userName, String password){
+        // Declare variables with validated username & hashed password
+        String validatedUserName = validateUserName(userName);
+        String hashedPassword = hashPassword(password);
+        boolean validSignUp = false; // response for successful sign-up, defaulted to false
+        connectSocket(); // Insure socket connection'
+        // Create Json payload
+        JsonObject jsonSignUpPayload = new JsonObject();
+        jsonSignUpPayload.addProperty("type", "signup");
+        jsonSignUpPayload.addProperty("username", validatedUserName);
+        jsonSignUpPayload.addProperty("password", hashedPassword);
         try {
-            dataOutputStream.writeUTF(jsonSignIn.toString());
+            dataOutputStream.writeUTF(jsonSignUpPayload.toString()); // Send Json payload to server
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        try {
+            String response = dataInputStream.readUTF();
+            JsonObject resPayload = JsonParser.parseString(response).getAsJsonObject();
+            String serverResponse = resPayload.get("successful").getAsString();
+            // Only if the server response is true, then sign-up is valid
+            if(serverResponse.equals("true")){ validSignUp = true;}
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return validSignUp;
+    }
+
+    public static String signIn(String userName, String password){
+        String hashedPassword = hashPassword(password);
+        connectSocket();
+        // Build Json payload
+        JsonObject jsonSignInPayload=new JsonObject();
+        jsonSignInPayload.addProperty("type","login");
+        jsonSignInPayload.addProperty("username", userName);
+        jsonSignInPayload.addProperty("password", hashedPassword);
+        try {
+            dataOutputStream.writeUTF(jsonSignInPayload.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -60,6 +95,13 @@ public class ClientServerHandler {
         return PlayerInfo.login;
     }
 
+    private static String validateUserName(String input){
+        // Regex to validate usernames -- standardized for no ._ combinations or at start or end of string
+        String regex = "^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.matches()){return input;} else return null;
+    }
     private static String hashPassword(String input){
         try {
             // Call SHA-1 Algorithm
