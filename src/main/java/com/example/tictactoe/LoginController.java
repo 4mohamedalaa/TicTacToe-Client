69ab299c22e770c9544f748609e61d4c8a5b695a
ArwaAhmed98 <arwaahmedmahmoud99@gmail.com>
2022-02-26 23:52:31 +0200

    NewScenesForLoginandSignUp+AnimationWorking

--- src/main/java/com/example/tictactoe/LoginController.java
+++ src/main/java/com/example/tictactoe/LoginController.java
@@ -1,19 +1,25 @@
 package com.example.tictactoe;
 
 import animatefx.animation.BounceInLeft;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
     @FXML
     Button loginBtn;
     @FXML
@@ -22,7 +28,51 @@ public class LoginController {
     PasswordField passwordField;
     @FXML
     Button switchToSignUp;
+    @FXML
+    ImageView logo;
+    @FXML
+    ImageView o1;
+    @FXML
+    ImageView o2;
+    @FXML
+    ImageView o3;
+    @FXML
+    ImageView x1;
+    @FXML
+    ImageView x2;
+    @FXML
+    ImageView x3;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        moveX(logo,-90);
+        moveY(o1,50);
+        moveY(o2,100);
+        moveY(o3,200);
+        moveY(x1,50);
+        moveY(x2,100);
+        moveY(x3,200);
+    }
+    public  void moveY(Node node,int valueY){
+        TranslateTransition translate=new TranslateTransition();
+        translate.setNode(node);
+        translate.setDuration(Duration.millis(1000)); // do the translate in 1 sec
+        // translate.setByX(valueX); // or 250
+        translate.setByY(valueY); // or 250
+        translate.setCycleCount(2); // repeat the translate 2 times
+        translate.setAutoReverse(true); // return to your  original position after translation
+        translate.play();
+    }
+    public  void moveX(Node node,int valueX){
+        TranslateTransition translate=new TranslateTransition();
+        translate.setNode(node);
+        translate.setDuration(Duration.millis(1000)); // do the translate in 1 sec
+        translate.setByX(valueX); // or 250
+        //  translate.setByX(valueY); // or 250
+        translate.setCycleCount(2); // repeat the translate 2 times
+        translate.setAutoReverse(true); // return to your  original position after translation
+        translate.play();
+    }
     public void loginBtnAction(ActionEvent event) throws IOException {
         System.out.println("Pressed sign-in button");
         // If the fields are NOT empty
@@ -75,4 +125,6 @@ public class LoginController {
     public String getPassword() {
         return passwordField.getText();
     }
+
+
 }
--- src/main/java/com/example/tictactoe/SignupController.java
+++ src/main/java/com/example/tictactoe/SignupController.java
@@ -37,16 +37,29 @@ public class SignupController implements Initializable {
     Button switchToLogin;
     @FXML
     ImageView logo;
+    @FXML
+    ImageView o1;
+    @FXML
+    ImageView o2;
+    @FXML
+    ImageView o3;
+    @FXML
+    ImageView x1;
+    @FXML
+    ImageView x2;
+    @FXML
+    ImageView x3;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        TranslateTransition translate=new TranslateTransition();
-        translate.setNode(logo);
-        translate.setDuration(Duration.millis(1000)); // do the translate in 1 sec
-        translate.setByX(-90); // or 250
-        translate.setCycleCount(2); // repeat the translate 2 times
-        translate.setAutoReverse(true); // return to your  original position after translation
-        translate.play();
+        moveX(logo,-90);
+        moveY(o1,50);
+        moveY(o2,100);
+        moveY(o3,200);
+        moveY(x1,50);
+        moveY(x2,100);
+        moveY(x3,200);
 
         RotateTransition rotate=new RotateTransition();
         rotate.setNode(logo);
@@ -55,12 +68,30 @@ public class SignupController implements Initializable {
         rotate.setByAngle(360);
         rotate.play();
 
-//        Duration sec = Duration.seconds(0.5);
-//        new BounceInRight(logo).setCycleDuration(4);
-//        Animation.setCycleDuration(Duration.millis(10));
+
+
 
     }
-
+    public  void moveY(Node node,int valueY){
+        TranslateTransition translate=new TranslateTransition();
+        translate.setNode(node);
+        translate.setDuration(Duration.millis(1000)); // do the translate in 1 sec
+       // translate.setByX(valueX); // or 250
+          translate.setByY(valueY); // or 250
+        translate.setCycleCount(2); // repeat the translate 2 times
+        translate.setAutoReverse(true); // return to your  original position after translation
+        translate.play();
+    }
+    public  void moveX(Node node,int valueX){
+        TranslateTransition translate=new TranslateTransition();
+        translate.setNode(node);
+        translate.setDuration(Duration.millis(1000)); // do the translate in 1 sec
+        translate.setByX(valueX); // or 250
+      //  translate.setByX(valueY); // or 250
+        translate.setCycleCount(2); // repeat the translate 2 times
+        translate.setAutoReverse(true); // return to your  original position after translation
+        translate.play();
+    }
     public void SignupBtnAction(ActionEvent event) throws IOException {
         System.out.println("Pressed action button");
         System.out.println(usrname.getText());
